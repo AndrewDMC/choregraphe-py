8809e0bf6ae207dd951da386ff2a5e0912f111f6
Lorenzo Barretta <43916566+DevLBD@users.noreply.github.com>
2022-04-28 16:11:25 +0200

    Python block template.
    Added template for the Python blocks.

---
+++ src/block.py
@@ -0,0 +1,29 @@
+from djitellopy import Tello
+
+class MyClass(GeneratedClass):
+    def __init__(self):
+        GeneratedClass.__init__(self)
+
+    def onLoad(self):
+        #put initialization code here
+        pass
+
+    def onUnload(self):
+        #put clean-up code here
+        pass
+
+    def onInput_onStart(self):
+        tello = Tello()
+        tello.connect()
+        tello.takeoff()
+        tello.move_up(150)
+        tello.move_down(80)
+        tello.move_up(50)
+        tello.move_down(80)
+        tello.move_up(50)
+        tello.land()
+        pass
+
+    def onInput_onStop(self):
+        self.onUnload() #it is recommended to reuse the clean-up as the box is stopped
+        self.onStopped() #activate the output of the box
