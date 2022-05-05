a1686f5a0685b7743fa680fd740a16f8aefbc08c
Andro434 <andro434.yt@gmail.com>
2022-05-05 13:44:59 +0200

    Begin work on the library.

---
+++ naotellopy/main.py
@@ -0,0 +1,12 @@
+import tello
+
+tello = Tello()
+
+tello.connect()
+tello.takeoff()
+tello.move_up(150)
+tello.move_down(80)
+tello.move_up(50)
+tello.move_down(80)
+tello.move_up(50)
+tello.land()
